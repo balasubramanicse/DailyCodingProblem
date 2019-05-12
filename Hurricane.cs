18c1ba815c288a9ebf0d4cfa73f7cfb44df95086
balasubramanicse <balasubramanicse@gmail.com>
2019-05-11 22:10:22 -0400

    Hurricane Problem

---
+++ Hurricane.cs
@@ -0,0 +1,37 @@
+﻿/*
+ An imminent hurricane threatens the coastal town of Codeville. If at most two people can fit in a rescue boat, and the maximum weight limit for a given boat is k, determine how many boats will be needed to save everyone.
+
+For example, given a population with weights [100, 200, 150, 80] and a boat limit of 200, the smallest number of boats required will be three.
+ */
+
+namespace DailyCodingProblem
+{
+    public class Hurricane
+    {
+        private readonly int[] PeopleData;
+        private readonly int BoatSize;
+        public Hurricane(int[] peopleData, int boatSize)
+        {
+            PeopleData = peopleData;
+            BoatSize = boatSize;
+        }
+
+        private int GetPeopleCount()
+        {
+            int totalPeople = 0;
+            for (int i = 0; i < PeopleData.Length; i++)
+            {
+                totalPeople = totalPeople + PeopleData[i];
+            }
+            return totalPeople;
+
+        }
+        public int GetBoatsNeeded()
+        {
+            int neededBoats = 0;
+            int totalPeople = GetPeopleCount();
+            neededBoats = totalPeople / BoatSize;
+            return totalPeople % BoatSize == 0 ? neededBoats : neededBoats + 1;
+        }
+    }
+}
